bbc8be4b616fe606a19f09a9c80691509efa7b5f
zyh <zhangyh@stpass.com>
2022-12-13 13:48:47 +0800

    feat: commit提交规范

---
+++ commitlint.config.js
@@ -0,0 +1,13 @@
+/*
+ * @Author: zyh
+ * @Date: 2022-12-13 11:37:36
+ * @LastEditors: zyh
+ * @LastEditTime: 2022-12-13 11:37:48
+ * @FilePath: /ChargeAccount/commitlint.config.js
+ * @Description: commit校验
+ *
+ * Copyright (c) 2022 by 穿越, All Rights Reserved.
+ */
+module.exports = {
+  extends: [ 'git-commit-emoji', 'cz' ],
+};
